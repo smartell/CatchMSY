faaa5bd652668bcdfc932ec89f2598fe6f20c92f
merrillrudd <merrillrudd@gmail.com>
2016-10-21 10:51:50 -0700

    updated for demonstrating R package

--- hakeDemo.R
+++ hakeDemo.R
@@ -1,165 +1,88 @@
 # Demo using the Nambibian hake data.
 # load the catchMSY library
 library(catchMSY)
-
+library(ggplot2)
+.THEME <- theme_bw(20)
 
 # Test that the model runs.
+## hake built in to the package
 catchMSYModel(hake)
 
 
+## parameter 1 is M -- lognormal prior with mean 0.2 and sd =1
+## parameter 2 is Fmsy -- uniform prior between 0 and 1
+## parameter 3 is MSY -- default normal prior
 # Change to uniform prior for msy
 hake$dfPriorInfo$dist[3]<-'unif'
-hake$dfPriorInfo$par1[3]<-100
-hake$dfPriorInfo$par2[3]<-400
+hake$dfPriorInfo$par1[3]<- quantile(hake$data$catch, 0.05)
+hake$dfPriorInfo$par2[3]<- quantile(hake$data$catch, 0.975)
 
 
 # Change selectivity
-hake$sel50 <- 4.0
-hake$sel95 <- 5.0
+hake$sel1 <- 4.0 ## age at 50% selectivity
+hake$sel2 <- 5.0 ## age at 95% selectivity
 
-# add length CV
-hake$la.cv <- 0.07
-
-# Generate ransome samples from dfPriorInfo
-hake <- sample.sid(hake,500)
-pairs(hake$S,gap=0)
+# Generate random samples from dfPriorInfo
+nsamp <- 500
+hake <- sample.sid(sID=hake,n=nsamp)
+pairs(hake$S,gap=0, pch=19)
 
 # Now run sir routine
-hake <- sir.sid(hake,1)
+hake <- sir.sid(hake,ncores=1)
+pairs(hake$S,gap=0,col=unlist(hake$code+1),pch=19)
 
 # Add Bo and h to samples
 ps <- cbind(hake$S,hake$bo,hake$h)
 colnames(ps) <- c("M","Fmsy","MSY","Bo","Steepness")
-pairs(ps,gap=0,col=unlist(hake$code+1),pch=20)
+pairs(ps,gap=0,col=unlist(hake$code+1),pch=19)
 
+### narrow down 'chosen' vs 'removed' samples without color specifications
+iclr <- c("steelblue","lightgrey")
+clr  <- iclr[unlist(sign(hake$code)+1)]
+pairs(hake$S,pch=19,gap=0,col=clr)
 
-# # library(tidyr)
-# # library(ggplot2)
-# # library(GGally)
-# # library(mvtnorm)
-# # library(foreach)
+## zoom in to B0 and steepness
+jpd  <- data.frame(Bo=hake$bo,Steepness=hake$h)
+colnames(jpd) <- c("Unfished biomass (Bo)","Steepness (h)")
+pairs(jpd,gap=0,pch=19)
+pairs(jpd,gap=0,pch=19,col=clr)
 
-# .THEME <- theme_bw(20)
+## scatterplot with marginal densities
+df<- with(hake,{
+	df <- data.frame(S,code=code,clr='black')
+	pdf <- subset(df,code==0)
+	ndf <- subset(df,code!=0)
+	ndf$clr <- 'red'
+	return(rbind(pdf,ndf))
+})
 
+p <- ggpairs(df,1:3,mapping=ggplot2::aes(color=clr,alpha=0.2))
+p <- ggpairs(df,1:3,upper = list(continuous = "density", combo = "box"))
+p <- ggpairs(df,1:3,upper = "blank",mapping=ggplot2::aes(color=clr,alpha=0.2))
+print(p + .THEME)
 
+## chosen depletion trajectories from possibilities
 
-# # 
-# # TEST CATCH ONLY METHOD USING hake0
-# # 
-# load("Hake_0.rda")
+dfd <- with(hake,{
+	df  <- ps.dt[code==0,]
+	colnames(df) = data$year
+	df <- cbind(id=1:dim(df)[1],df)
+	return(gather(df,key,dt,-id))
+})
 
+p <- ggplot(dfd,aes(x=as.double(key),y=dt)) 
+p <- p + stat_summary(geom="ribbon",fun.ymin="min",fun.ymax="max",alpha=0.1)
+p <- p + stat_summary(geom="ribbon", fun.data="median_hilow",alpha=0.2,fill="blue")
+p <- p + stat_summary(geom="ribbon", fun.data="mean_cl_boot",alpha=0.2,fill="red")
+p <- p + geom_line(aes(group=id),size=0.1,alpha=0.1)
+p <- p + labs(x="Year",y="Depletion") + ylim(0,1)
+print(p + .THEME)
 
-
-
-# hake0 <- sample.sid(hake0,500)
-# hake0 <- sir.sid(hake0,1)
-
-# with(hake0,{
-# 	matplot(t(ps.dt[code==0,]),type="l",ylim=c(0,1))
-# 	matplot(t(ps.bt[code==0,]),type="l")
-# 	matplot(t(ps.ft[code==0,]),type="l",col="red",lty=1)
-# })
-
-# iclr <- c("steelblue","lightgrey")
-# clr  <- iclr[unlist(sign(hake0$code)+1)]
-# jpd  <- data.frame(Bo=hake0$bo,Steepness=hake0$h)
-# colnames(jpd) <- c("Unfished biomass (Bo)","Steepness (h)")
-# pairs(hake0$S,pch=20,gap=0)
-# pairs(hake0$S,pch=20,gap=0,col=clr)
-# pairs(jpd,gap=0,pch=20)
-# pairs(jpd,gap=0,pch=20,col=clr)
-
-
-
-# df2 <- with(hake0,{
-# 	df  <- ps.dt[code==0,]
-# 	colnames(df) = data$year
-# 	df <- cbind(id=1:dim(df)[1],df)
-# 	return(gather(df,key,dt,-id))
-# })
-
-
-
-
-# p <- ggplot(df2,aes(x=as.double(key),y=dt)) 
-# p <- p + stat_summary(geom="ribbon",fun.ymin="min",fun.ymax="max",alpha=0.1)
-# p <- p + stat_summary(geom="ribbon", fun.data="median_hilow",alpha=0.2,fill="blue")
-# p <- p + stat_summary(geom="ribbon", fun.data="mean_cl_boot",alpha=0.2,fill="red")
-# p <- p + geom_line(aes(group=id),size=0.1,alpha=0.1)
-# p <- p + labs(x="Year",y="Depletion") + ylim(0,1)
-# print(p + .THEME)
-
-# posts.df <- posts.df %>% gather(key,ssb,-id,-wts)
-
-# df<- with(hake0,{
-# 	df <- data.frame(S,code=code,clr='black')
-# 	pdf <- subset(df,code==0)
-# 	ndf <- subset(df,code!=0)
-# 	ndf$clr <- 'red'
-# 	return(rbind(pdf,ndf))
-# })
-
-# # ggpairs(df,1:3,color='clr')
-# # p <- ggpairs(df,1:3,color='clr',upper = list(continuous = "density", combo = "box"))
-# p <- ggpairs(df,1:3,color='clr',upper = "blank",alpha=0.2)
-# print(p + .THEME)
-
-
-# Distribution for MSY
-# msy1 <- hake0$S[hake0$code==0,3]
-
-
-# df<-with(hake0,{
-# 	df <- data.frame(Year=data$year,Depl=ps.dt[,code==0])
-# 	return(df)
-# })
-
-# gdf <- df %>% gather(Year,value="Depletion") 
-# colnames(gdf) <- c("Year","Sample","Depletion")
-
-
-# p <- ggplot(gdf,aes(x=Year,y=Depletion)) 
-# p <- p + stat_summary(geom="ribbon",fun.ymin="min",fun.ymax="max",alpha=0.1)
-# p <- p + stat_summary(geom="ribbon", fun.data="median_hilow",alpha=0.2)
-# p <- p + stat_summary(geom="ribbon", fun.data="mean_se",alpha=0.2,fill="red")
-# p <- p + stat_summary(geom="ribbon", fun.data="mean_cl_normal",alpha=0.2,fill="red")
-# p <- p + geom_line()
-
-# print(p)
-
-# p  <- ggplot(ndf,aes(y=Spawning.Biomass,x=Year))
-# 	p  <- p + stat_summary(geom="ribbon", fun.ymin="min", fun.ymax="max",alpha=0.3,fill=clr)
-# 	p  <- p + geom_line(aes(Year,y=Spawning.Biomass),data=mdf)
-# 	p  <- p + ylim(c(0,max(ndf$Spawning.Biomass)))
-# 	return(p + .THEME)
-
-
-
-# hake0 <- runModel(hake0)
-# plot(hake0)
-
-
-
-
-
-# # Create a new sID object
-# # new_sID(id="Namibian Hake",dfile="NamibianHake.dat")
-
-# # load the hake sID object and assign to hake
-# data(NamibianHake)
-
-
-# # estimate parameters
-# hake <- solver(hake)
-
-
-# # run the age structured model using defaults
-# hake <- runModel(hake)
-
-# # Plot the results
-# plot(hake)
-
-
-
-
-
+## MSY histogram
+par(mfrow=c(1,1))
+xlim <- c(0, hake$dfPriorInfo$par2[3]*1.2)
+ylim <- c(0, nsamp/5)
+hist(hake$S[,"msy"], col="gray", lty="blank", xlim=xlim, ylim=ylim, xlab="MSY", ylab="Frequency", main="")
+par(new=TRUE)
+hist(hake$S[hake$idx,"msy"], col="#AA000050", lty="blank", xlim=xlim, ylim=ylim, xlab="", ylab="", main="")
+legend("topright", legend=c("Sampling space", "Catch only"), pch=15, col=c("gray", "#AA000050"), cex=2)
